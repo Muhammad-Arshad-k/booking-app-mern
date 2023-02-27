3415c845ad92f58946883a5d8164ae8758a0d3e7
Muhammad Arshad K <muhammadarshad8935@gmail.com>
2023-02-27 15:53:15 +0530

    modal for room choosing

--- frontend/src/components/reserve/Reserve.jsx
+++ frontend/src/components/reserve/Reserve.jsx
@@ -2,10 +2,22 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import useFetch from '../../hooks/useFetch';
+import { useState } from 'react';
 
 const   Reserve=({setOpen,hotelId}) =>{
+
+  const[selectedRooms,setSelectedRooms] =useState([])
   
-  const {data,loading,error}= useFetch(`hotels/room/${hotelId}`)
+  const {data,loading,error}= useFetch(`hotels/room/${hotelId}`);
+  const handleSelect = (e)=>{
+    const selected = e.target.checked;
+    const value    = e.target.value;
+
+    setSelect(checked
+      ?[...selectedRooms,value]
+      :selectedRooms.filter((item)=>item!==value))
+   
+  }
 
   return ( 
    <div className="reserve">
@@ -22,7 +34,10 @@ const   Reserve=({setOpen,hotelId}) =>{
              </div>
              <div>
               {item.roomNumbers.map(roomNumber=>{
+                <div className="room">
                 <label htmlFor="">{roomNumber.number}</label>
+                <input type="checkbox" value={roomNumber._id} onChange={handleSelect}/>
+                </div>
               })}
              </div>
           </div>
